b81ecf5ca5c4e9b15f356ae6599e7098405da34c
LombardiGiacomo <lombardi.giacomo23@gmail.com>
2025-07-25 17:54:21 +0200

    prova

--- js/main.js
+++ js/main.js
@@ -4,5 +4,29 @@ const hamburger = document.querySelector('.nav-hamburger');
 hamburger.addEventListener('click', function() {
   hamburger.classList.toggle('open');
   menu.classList.toggle('open');
+
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+
+});
+
+const menuLinks = document.querySelectorAll('.menuMobile a');
+
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Chiudi il menu
+    menu.classList.remove('open');
+    hamburger.classList.remove('open');
+
+    // Vai alla sezione
+    const targetId = this.getAttribute('href');
+    const target = document.querySelector(targetId);
+
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
 });
 
